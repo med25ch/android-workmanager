ffd4d5335245cdc917421282e3d908f439fe3ad9
MCH <70763505+mohamed25ch@users.noreply.github.com>
2023-05-22 18:49:46 -0400

    Tag and display Work status using LiveData

--- app/src/main/java/com/example/background/BlurViewModel.kt
+++ app/src/main/java/com/example/background/BlurViewModel.kt
@@ -20,6 +20,7 @@ import android.app.Application
 import android.content.ContentResolver
 import android.content.Context
 import android.net.Uri
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.work.*
@@ -34,6 +35,16 @@ class BlurViewModel(application: Application) : ViewModel() {
     internal var outputUri: Uri? = null
 
     private val workManager = WorkManager.getInstance(application)
+
+    // New instance variable for the WorkInfo
+    internal val outputWorkInfos: LiveData<List<WorkInfo>>
+
+    init {
+        imageUri = getImageUri(application.applicationContext)
+        // This transformation makes sure that whenever the current work Id changes the WorkInfo
+        // the UI is listening to changes
+        outputWorkInfos = workManager.getWorkInfosByTagLiveData(TAG_OUTPUT)
+    }
 
     init {
         imageUri = getImageUri(application.applicationContext)
@@ -66,7 +77,9 @@ class BlurViewModel(application: Application) : ViewModel() {
         }
 
         // Add WorkRequest to save the image to the filesystem
-        val save = OneTimeWorkRequest.Builder(SaveImageToFileWorker::class.java).build()
+        val save = OneTimeWorkRequestBuilder<SaveImageToFileWorker>()
+            .addTag(TAG_OUTPUT)
+            .build()
 
         continuation = continuation.then(save)
 
